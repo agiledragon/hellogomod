e9234ee2b4312a56b95e00e9548c8e1df3fb9ddd
agiledragon <zhangxiaolong1980@126.com>
2020-06-25 21:16:37 +0800

    big

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/agiledragon/hellogomod
+module github.com/agiledragon/hellogomod/v2
 
 go 1.14
+
+require github.com/agiledragon/hellogomod v1.0.0 // indirect
--- hello.go
+++ hello.go
@@ -2,6 +2,13 @@ package hellogomod
 
 import "fmt"
 
-func Hello(who string) string {
-	return fmt.Sprintf("Hello, %s!", who)
+func Hello(who, t string) string {
+	switch t {
+    case "en":
+        return fmt.Sprintf("Hello, %s!", who), nil
+    cse "cn":
+        return fmt.Sprintf("你好, %s!", who), nil
+    default:
+        return "", errors.New("unknown language")
+    }
 }
